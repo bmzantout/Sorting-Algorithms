8c8a9d05354cd3dbc8173dd35b825d85e6d9edbd
Basme Zantout <105554803+bmzantout@users.noreply.github.com>
2023-04-13 12:58:33 +0300

    Update Merge.java

--- Question 1/Merge.java
+++ Question 1/Merge.java
@@ -9,9 +9,6 @@ public class Merge {
 	//-----------------------------------------------------
 	// Title: Part 1
 	// Author: Basme Zantout
-	// ID: 99227947762
-	// Section: 1
-	// Assignment: 2
 	// Description: This class includes the Merge Sort Algorithm
 	//-----------------------------------------------------
 
